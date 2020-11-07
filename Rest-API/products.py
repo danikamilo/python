1ac2609f94acdf97b4b39898549bbaf7ec727531
danikamilo <danikamilo777@gmail.com>
2020-11-07 10:42:17 -0500

    Delete products.py

--- Rest-API/products.py
+++
@@ -1,5 +0,0 @@
-products = [
-    {"name" : "laptop", "price" : 800, "quantity" : 4},
-    {"name" : "mouse", "price" : 40, "quantity" : 4},
-    {"name" : "moitor", "price" : 400, "quantity" : 4}
-]
